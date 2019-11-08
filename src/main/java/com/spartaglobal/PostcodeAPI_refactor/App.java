e3576ef6e005a00d4361c9bba3f69a0360fcf2a6
Dana Korang-Awua <danaawua@hotmail.co.uk>
2019-11-08 11:20:39 +0000

    Initial commit

---
+++ src/main/java/com/spartaglobal/PostcodeAPI_refactor/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.PostcodeAPI_refactor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
